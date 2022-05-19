4d168165e855b0db3dd498455cac9b6d887f0c66
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2022-05-19 07:48:10 +0200

    roast

---
+++ roasted.reviews/ShopData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roasted.reviews
+{
+    internal class ShopData
+    {
+        public static (string name, string id) shop;
+        public static (string address, string email, string phoneNum) contact;
+        public static (string overall, string[] category) results;
+        public static (string[] item, string[] prize, string[] rating) menu;
+        public static bool[,] hours = new bool[7, 48]; 
+    }
+}
